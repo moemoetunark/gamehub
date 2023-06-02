e7a2ae02a9f724f0f8e08ed56436c6be7bab233b
MR MOE MOE TUN <117424385+moemoetunark@users.noreply.github.com>
2023-06-02 18:53:53 +0630

    Displaying Platform Icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,7 +11,7 @@ export const GameGrid = () => {
   return (
     <>
     {error&&<Text>{error}</Text>}
-    <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} spacing={10}>{games.map(game =>
+    <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 4}} padding='10px' spacing={10}>{games.map(game =>
     <GameCard key={game.id} game={game}/>)}
     </SimpleGrid>
     </>
